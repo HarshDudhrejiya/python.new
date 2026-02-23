08dbb5043c73675507067cb11db672b56cdb25b5
HarshDudharejiya <dudharejiyaharsh19@gmail.com>
2026-02-23 22:35:51 +0530

    first commit

---
+++ casting.py
@@ -0,0 +1,25 @@
+# string ne int ma casting karva mate 
+# a = 10
+# b = "5"
+
+# c = int(b)
+# sum = (a+c)
+# print(sum)
+# print(type(sum))
+
+
+
+# a = 10
+# b = str(a)
+
+# print (type(b))
+
+
+
+name = (input("enter your name :"))
+age  = int(input("enter your ege"))
+weight = (input("enter your weight"))
+
+print("hi welcome !",name)
+print("ohh you are old",age)
+print("gret",weight)
